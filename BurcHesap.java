eb6616cfa6255248ad70e63fbc30963378c4310c
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2023-01-06 14:21:38 +0300

    Update BurcHesap.java

--- BurcHesap.java
+++ BurcHesap.java
@@ -14,6 +14,8 @@ public class Main {
             System.out.println("Ayın hangi gününde doğdunuz?");
             gun = Input.nextInt();
         }
+        else System.out.println("Ocak = 1, Şubat = 2, Mart = 3, Nisan = 4, Mayıs = 5, Haziran = 6" +
+                ", Temmuz = 7, Ağustos = 8, Eylül = 9, Ekim = 10, Kasım = 11, Aralık =12");
 
         switch (ay)
         {
